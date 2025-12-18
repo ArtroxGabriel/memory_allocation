1a142d016891574fbe881acdd9cf9c939ac5bcef
erikbayerlein <erik.bayer@icloud.com>
2025-12-18 19:25:18 -0300

    feat: adding parser command vclidation

---
+++ src/main/java/org/example/Parser/Validator/ValidationResult.java
@@ -0,0 +1,23 @@
+package org.example.Parser.Validator;
+
+import lombok.Getter;
+
+@Getter
+public class ValidationResult {
+    private final boolean valid;
+    private final String errorMessage;
+
+    private ValidationResult(boolean valid, String errorMessage) {
+        this.valid = valid;
+        this.errorMessage = errorMessage;
+    }
+
+    public static ValidationResult success() {
+        return new ValidationResult(true, null);
+    }
+
+    public static ValidationResult failure(String message) {
+        return new ValidationResult(false, message);
+    }
+
+}
