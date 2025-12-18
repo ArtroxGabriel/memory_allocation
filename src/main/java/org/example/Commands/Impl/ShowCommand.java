569a3a040930c9453251c3e112fce1abd5a0cbb0
Gabrigas <artroxgabriel@gmail.com>
2025-12-18 19:25:50 -0300

    feat: implements the memory show command

--- src/main/java/org/example/Commands/Impl/ShowCommand.java
+++ src/main/java/org/example/Commands/Impl/ShowCommand.java
@@ -14,8 +14,34 @@ public class ShowCommand extends AbstractCommand {
         if (!memory.isInitialized()) {
             return CommandsResult.Failure("Memory is not initialized.");
         }
+        var out = new StringBuilder();
+        var memorySize = memory.getMemory().size();
+        out.append("Memory Map (").append(memorySize).append(" bytes )\n");
 
-        return CommandsResult.Success("TODO: MUST BE IMPLEMENTED");
+        var sepLine = "-".repeat(Math.max(0, memorySize + 2));
+        out.append(sepLine);
+        out.append("\n[");
+        for (int i = 0; i < memorySize; i++) {
+            if (memory.isFree(i)) {
+                out.append(".");
+            } else {
+                out.append("#");
+            }
+        }
+        out.append("]\n");
+
+        out.append("[");
+        for (int i = 0; i < memorySize; i++) {
+            if (memory.isFree(i)) {
+                out.append(".");
+            } else {
+                out.append(memory.getMemory().get(i).getId());
+            }
+        }
+        out.append("]\n");
+        out.append(sepLine);
+
+        return CommandsResult.Success(out.toString());
     }
 
     @Override
